95f400415e7d439c3597d216cb8ac028937c0db9
iberiaz9 <alvarado.ja53@gmail.com>
2023-05-11 14:15:02 -0700

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,2 +1,17 @@
-import math
-print(f"Square root of 4 is {math.sqrt(4)}")
+import random
+
+students = ['Tomas','Sher','Sal','Jose','Ryan','Lawrence','Mel','Senay','Christie','Vincent','Caitlyn','Adeline','Ray','Mike']
+
+def pick(my_students):
+    my_students.remove(student := random.choice(my_students))
+    return student
+
+more = 'y'
+
+while students and len(more) and not more[0] in 'nN':
+    winner = pick(students)
+    more = input("And the winner is: " + winner + "! More? [y]/n: ")
+    if not len(more):
+        more = 'y'
+
+print('Thanks for playing!')
