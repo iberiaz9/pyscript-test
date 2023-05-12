508e4d97c3e9fc1f77e9c4aedad98eeb83d67a3d
iberiaz9 <alvarado.ja53@gmail.com>
2023-05-11 17:26:06 -0700

    Changes to run on web server

--- demo1.py
+++ demo1.py
@@ -1,19 +1,24 @@
-import random, time
+import random
+from pyscript import Element
 
-students = ['Tomas','Sher','Sal','Jose','Ryan','Lawrence','Mel','Senay','Christie','Vincent','Caitlyn','Adeline','Ray','Mike']
+
+students = ['Tomas','Sher','Sal','Jose','Ryan','Lawrence','Mel','Senay','Christie',
+            'Vincent','Caitlyn','Adeline','Ray','Mike']
 
 def pick(my_students):
     my_students.remove(student := random.choice(my_students))
     return student
 
-more = 'y'
+def my_function():
+    more = Element('test-input').value
+    if more and more[0].lower() == 'n':
+            students[:] = []
+    if students:
+        winner = pick(students)
+        display("And the winner is: " + winner + "!", target="helloDiv")
+    else:
+        display("No more students!", target="helloDiv")
+        
 
-while students and len(more) and not more[0] in 'nN':
-    winner = pick(students)
-    print("And the winner is: " + winner + "!")
-    time.sleep(5)
-    more = input("More? [y]/n: ")
-    if not len(more):
-        more = 'y'
-
-print('Thanks for playing!')
+winner = pick(students)
+display("And the winner is: " + winner + "!", target="helloDiv")
